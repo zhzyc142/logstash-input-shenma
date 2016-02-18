11296423eb5cd155be43963e3a1fb26ba785ff1b
zhaoyc <zhzyc142@163.com>
2016-02-18 14:23:45 +0800

    debug

--- lib/logstash/inputs/example.rb
+++ lib/logstash/inputs/example.rb
@@ -85,7 +85,7 @@ class LogStash::Inputs::Example < LogStash::Inputs::Base
 
   def run(queue)
     @logger.info("run action:")
-    @logger.info(queue.to_json)
+    @logger.info(queue)
     if @schedule
       @scheduler = Rufus::Scheduler.new(:max_work_threads => 1)
       @scheduler.cron @schedule do
@@ -114,7 +114,7 @@ class LogStash::Inputs::Example < LogStash::Inputs::Base
     @parameters['sql_last_value'] = @sql_last_value
     execute_statement(@statement, @parameters) do |row|
       @logger.info("execute_query callback action:")
-      @logger.info(row.to_json)
+      @logger.info(row)
       event = LogStash::Event.new(row)
       decorate(event)
       queue << event
