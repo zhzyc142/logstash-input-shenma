69799783ade3e33893150f5ff2085f2350e4f8e7
zhaoyc <zhzyc142@163.com>
2016-02-19 10:52:28 +0800

    debug

--- lib/logstash/inputs/shenma.rb
+++ lib/logstash/inputs/shenma.rb
@@ -120,7 +120,7 @@ class LogStash::Inputs::Shenma < LogStash::Inputs::Base
 
       @logger.error("execute_query callback action #{row}")
       if(row["userid"] && row["userid"]!=0)
-        row["isLogin"] = is_login?(row["userid"], (Date.today()-1).utc,  Date.today().utc)
+        row["isLogin"] = is_login?(row["userid"], (Date.today()-1).to_time,  Date.today().to_time)
         row["sendMessage"] = buyer_send_message_number(row["userid"], (Date.today()-1).to_time,  Date.today().to_time)
         row["receivedMessage"] = buyer_received_message_number(row["userid"], (Date.today()-1).to_time,  Date.today().to_time)
         event = LogStash::Event.new(row)
