94b2f8847c5707dc308ecac8168a323503aa05ca
zhaoyc <zhzyc142@163.com>
2016-02-23 16:13:10 +0800

    debug

--- lib/logstash/inputs/shenma.rb
+++ lib/logstash/inputs/shenma.rb
@@ -248,7 +248,7 @@ class LogStash::Inputs::Shenma < LogStash::Inputs::Base
       }
     }
     client = Elasticsearch::Client.new(:host => @jdbc_ecs_host)
-    res = client.search({body: query, index: "esmapping%2FESUserVisitPage" })
+    res = client.search({body: query, index: "esmapping", type:"ESUserVisitPage" })
     @logger.error("ecs result #{res}")
 
     if res && res["aggregations"] && res["aggregations"]["stat_date"] && res["aggregations"]["stat_date"]["buckets"] && res["aggregations"]["stat_date"]["buckets"].class == Array
