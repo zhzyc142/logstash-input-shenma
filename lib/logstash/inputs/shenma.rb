37edec0d4f5b62b007c7b6139ad82e6e7f9572f9
zhaoyc <zhzyc142@163.com>
2016-02-23 09:18:35 +0800

    debug

--- lib/logstash/inputs/shenma.rb
+++ lib/logstash/inputs/shenma.rb
@@ -156,9 +156,7 @@ class LogStash::Inputs::Shenma < LogStash::Inputs::Base
         row["time_end"] = Date.parse(time_end).to_s
         row["orderamount"] = row["orderamount"].to_f
         row["orderrecivedamount"] = row["orderrecivedamount"].to_f
-        @logger.error("run action callback #{row['userLevel']}")
-        @logger.error("run action callback #{row['userlevel']}")
-        row["userLevel"] = (row["userLevel"].to_i == 4 ? "专柜买手" : (row["userLevel"].to_i == 8 ? "认证买手" : (row["userLevel"].to_i == 16 ? "品牌买手" : "未知类型")  ))
+        row["userLevel"] = (row["userlevel"].to_i == 4 ? "专柜买手" : (row["userlevel"].to_i == 8 ? "认证买手" : (row["userlevel"].to_i == 16 ? "品牌买手" : "未知类型")  ))
         event = LogStash::Event.new(translate_name(row, "buyer_everyday_data"))
         decorate(event)
         queue << event
